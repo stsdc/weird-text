40c50cd707e88f82f2d41b886f9efced8462507e
stsdc <6031763+stsdc@users.noreply.github.com>
2022-01-06 22:28:52 +0100

    adds encoding capability

---
+++ encoder.py
@@ -0,0 +1,38 @@
+import re
+import random
+
+class Encoder:
+    separator = '\n—weird—\n'
+    
+    def __init__(self):
+        self.tokenize_re = re.compile(r'(\w+)', re.U)
+
+        self.word_list = list()
+
+    def encode(self, data_in: str) -> str:
+        """
+        :param data: 
+        :return:
+        """
+
+        data_out = data_in
+
+        # self.word_list = re.findall(self.tokenize_re, data)
+        for match in re.finditer(self.tokenize_re, data_in):
+            word = match.group()
+            self.word_list.append(word)
+
+            word_shuffled = self._shuffle(word)
+
+            data_out = data_out.replace(word, word_shuffled)
+
+            print(f'{word} -> {word_shuffled}')
+        
+
+        return self.separator + data_out + self.separator
+
+
+    def _shuffle(self, word: str) -> str:
+        word_characters = list(word)
+        random.shuffle(word_characters)
+        return ''.join(word_characters)
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python
+
+from encoder import Encoder
+
+if __name__ == '__main__':
+    encoder = Encoder()
+
+    encoded_text = encoder.encode('This is a long looong test sentence,\nwith some big (biiiiig) words!')
+    print(encoded_text)
