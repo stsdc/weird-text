655603f46ee4f0697ee1d27166b669b8e75367d2
stsdc <6031763+stsdc@users.noreply.github.com>
2022-01-08 20:09:51 +0100

    add more exceptions

--- decoder.py
+++ decoder.py
@@ -5,10 +5,21 @@ from decoder_exceptions import *
 class Decoder:
     separator = '\n-weird-\n'
 
-    def __init__(self, word_list: str) -> None:
+    def __init__(self, word_list: list) -> None:
         self.re_all_words = re.compile(r'(\w+)', re.U)
         self.re_all_encoded_words = re.compile(r'(?:-weird-)', re.U)
         self.word_list = word_list
+
+    @property
+    def word_list(self) -> str:
+        return self._word_list
+
+    @word_list.setter
+    def word_list(self, word_list: str) -> None:
+        if len(word_list) == 0:
+            raise EmptyWordListError()
+        else:
+            self._word_list = word_list
 
     def decode(self, data_in: str) -> str:
         """
@@ -34,8 +45,6 @@ class Decoder:
 
 
         
-        # print(data_out)
-
     def _extract_encoded_text(self, data_in: str) -> str:
         if self.separator in data_in:
             data_to_decode = data_in.split(self.separator)
@@ -48,15 +57,19 @@ class Decoder:
             if len(word) == len(encoded_word):
                 number_of_confirmed_chars = 0
 
-                # checking for the same characters;
-                # it will not take in count if there
-                # are more than one same character
-                # TODO: remove chars after confirming
+                word_copy = word
                 for char in encoded_word:
-                    if char in word:
+                    if char in word_copy:
                         number_of_confirmed_chars += 1
+
+                        # Removing confirmed chars. It will prevent false
+                        # positives in the next iterations.
+                        # (e.g. 'aaabc' and 'cba' will return aaabc as a match)
+                        word_copy = word_copy.replace(char, '', 1)
 
                 if number_of_confirmed_chars == len(encoded_word):
                     # print (f'{encoded_word} -> {word}')
                     return word
+
+        raise NoMatchWordFoundError()
 
--- decoder_exceptions.py
+++ decoder_exceptions.py
@@ -8,3 +8,19 @@ class NoSeparatorError(DecoderError):
     def __init__(self, message="Error: No separator found."):
         self.message = message
         super().__init__(self.message)
+
+class EmptyWordListError(DecoderError):
+    """
+    Raised when the passed word list is empty.
+    """
+    def __init__(self, message="Error: Word list is empty."):
+        self.message = message
+        super().__init__(self.message)
+
+class NoMatchWordFoundError(DecoderError):
+    """
+    Raised when no match found for an encoded word in word list.
+    """
+    def __init__(self, message="Error: No match found for encoded word in word list."):
+        self.message = message
+        super().__init__(self.message)
