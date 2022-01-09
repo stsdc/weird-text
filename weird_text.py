73d8f998952624e3c60f5caf4b29f4b1beada199
stsdc <6031763+stsdc@users.noreply.github.com>
2022-01-09 14:53:26 +0100

    change text for encoder

--- weird_text.py
+++ weird_text.py
@@ -6,7 +6,7 @@ from decoder import Decoder
 if __name__ == '__main__':
     encoder = Encoder()
 
-    encoded_text = encoder.encode('This is a long looong test sentence,\nwith some big (biiiiig) words!')
+    encoded_text = encoder.encode('Lorem Ipsum is simply dummy text of the printing and typesetting industry.')
     print(encoded_text)
 
     decoder = Decoder(encoder.word_list)
